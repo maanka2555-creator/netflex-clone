bd59062571c09c2b77a5b3e0d4b2bad1b7e4401a
maanka <maanka2555@gmail.com>
2025-12-19 12:03:12 +0300

    last change

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useRef } from "react";
 import "./Navbar.css";
-import logo from "../../assets/Logo.png";
+import logo from "../../assets/logo.png";
 import search_icon from "../../assets/search_icon.svg";
 import bell_icon from "../../assets/bell_icon.svg";
 import profile_img from "../../assets/profile_img.png";
 import caret_icon from "../../assets/caret_icon.svg";
-import { logout } from "../../Firebase";
+import { logout } from "../../firebase";
 const Navbar = () => {
 
   const navRef = useRef()
