5400ac8336fdb6aeae3be2f557c4c836b7cdb2d5
maanka <maanka2555@gmail.com>
2025-12-19 12:00:02 +0300

    login firebase import change

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
-import { login,signup } from '../../Firebase'
+import { login,signup } from '../../firebase'
 import netflix_spinner from '../../assets/netflix_spinner.gif'
 
 const Login = () => {
